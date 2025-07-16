5747966cba6571306210ec23f06ef5bdb5ee4c99
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-16 14:13:30 +0500

    Models and migrations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [ 
+        'title',
+        'slug',
+        'description',
+        'thumbnail',
+        'tech_stack',
+        'github_link',
+        'live_link',
+    ];
 }
---
+++ database/migrations/2025_07_16_090808_create_case_studies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('case_studies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('summary');
+            $table->longText('content');
+            $table->string('thumbnail')->nullable();
+            $table->string('tech_stack')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('case_studies');
+    }
+};
