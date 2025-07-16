2a4ed900d0e2ccfa3d9ae6a882faceaf757c223b
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-16 13:20:01 +0500

    Controllers and Projects Setup initals

--- app/Http/Controllers/CaseStudyController.php
+++ app/Http/Controllers/CaseStudyController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class CaseStudyController extends Controller
 {
-    //
+    public function index() 
+    {
+        $project = Project::latest()->get();
+        return view('projects.index', compact('projects'));
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->firstOrFail();
+        return view('projects.show', compact('project'));
+    }
 }
